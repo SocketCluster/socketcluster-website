282bd4461933b1fe82557c85eb12acea9e591d28
Nelson Zheng <nelsonjzheng@gmail.com>
2014-10-20 00:57:12 +1100

    big bad commit

---
+++ public/assets/js/app.js
@@ -0,0 +1,94 @@
+var socketclusterApp = angular.module('socketclusterApp', ['ngRoute']);
+
+socketclusterApp.config(function($routeProvider) {
+  $routeProvider
+
+    // Route for Home page
+    .when('/', {
+      templateUrl: 'app/views/index.html',
+      controller: 'mainController'
+    })
+
+    // Route for Demos
+    .when('/demos', {
+      templateUrl: 'app/views/demos.html',
+      controller: 'demosController'
+    })
+    
+    // Route for Getting Started
+    .when('/docs/getting-started', {
+      templateUrl: 'app/views/docs/getting-started.html',
+      controller: 'docsController'
+    })
+    // Route for Basic Setup
+    .when('/docs/basic-usage', {
+      templateUrl: 'app/views/docs/basic-usage.html',
+      controller: 'docsController'
+    })
+    // Route for Performance
+    .when('/performance', {
+      templateUrl: 'app/views/performance.html',
+      controller: 'performanceController'
+    })
+    .when('/docs/introduction', {
+      templateUrl: 'app/views/docs/introduction.html',
+      controller: 'docsController'
+    })
+    //
+    .when('/docs/middleware-and-authorization', {
+      templateUrl: 'app/views/docs/middleware.html',
+      controller: 'docsController'
+    })
+    //
+    .when('/docs/handling-failure', {
+      templateUrl: 'app/views/docs/handling-failure.html',
+      controller: 'docsController'
+    })
+    //
+    .when('/docs/full-stack', {
+      templateUrl: 'app/views/docs/full-stack.html',
+      controller: 'docsController'
+    })
+    // Route for Docs
+    .when('/docs', {
+      templateUrl: 'app/views/docs/introduction.html',
+      controller: 'docsController'
+    });
+});
+
+socketclusterApp.controller('mainController', function($scope) {
+  $scope.ctrl = 'mainController';
+});
+
+socketclusterApp.controller('demosController', function($scope) {
+
+});
+
+socketclusterApp.controller('gsController', function($scope) {
+
+});
+
+socketclusterApp.controller('bsController', function($scope) {
+
+});
+
+socketclusterApp.controller('middlewareController', function($scope) {
+
+});
+
+socketclusterApp.controller('performanceController', function($scope) {
+
+});
+
+socketclusterApp.controller('docsController', function($scope) {
+
+});
+
+socketclusterApp.directive('prettyprint', function() {
+  return {
+    restrict: 'C',  
+    link: function postLink(scope, element, attrs) {
+      prettyPrint();
+    }
+  }
+});
--- public/js/app.js
+++
@@ -1,6 +0,0 @@
-var socketclusterApp = angular.module('socketclusterApp', []);
-
-socketclusterApp.controller('mainController', function($scope) {
-
-  $scope.message = 'Everyone come and see how good I look!';
-});

8271c50374fb69a8b22719671f898e186e541782
sacOO7 <sachinshinde7676@gmail.com>
2020-03-11 21:48:06 +0530

    added updated client libraries in grid-block, supporting latest sc-version

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -203,6 +203,61 @@ class Index extends React.Component {
       </Block>
     );
 
+
+      const ClientLibraries = () => (
+          <BlockWithHeading layout="fourColumn" background={'light'} heading={'Client Libraries'}>
+              {[
+                  {
+                      image: 'https://image.flaticon.com/icons/svg/136/136530.svg',
+                      imageAlign: 'top',
+                      title: 'Javascript Client',
+                      imageLink: 'https://github.com/SocketCluster/socketcluster-client'
+                  },
+                  {
+                      image: 'https://image.flaticon.com/icons/svg/226/226770.svg',
+                      imageAlign: 'top',
+                      title: 'Java/Android Client',
+                      imageLink: 'https://github.com/sacOO7/socketcluster-client-java'
+                  },
+                  {
+                      image: 'https://image.flaticon.com/icons/svg/180/180867.svg',
+                      imageAlign: 'top',
+                      title: 'Python Client',
+                      imageLink: 'https://github.com/sacOO7/socketcluster-client-python'
+                  },
+                  {
+                      image: 'https://image.flaticon.com/icons/svg/376/376105.svg',
+                      imageAlign: 'top',
+                      title: 'DotNet Client',
+                      imageLink: 'https://github.com/sacOO7/SocketclusterClientDotNet'
+                  },
+                  {
+                      image: 'https://github.com/egonelbre/gophers/raw/master/.thumb/animation/gopher-dance-long-3x.gif',
+                      imageAlign: 'top',
+                      title: 'Go Client',
+                      imageLink: 'https://github.com/sacOO7/socketcluster-client-go'
+                  },
+                  {
+                      image: 'https://image.flaticon.com/icons/svg/919/919833.svg',
+                      imageAlign: 'top',
+                      title: 'Swift/iOS Client',
+                      imageLink: 'https://github.com/sacOO7/socketcluster-client-swift'
+                  },
+                  {
+                      image: 'https://image.flaticon.com/icons/svg/919/919842.svg',
+                      imageAlign: 'top',
+                      title: 'Ruby client',
+                      imageLink: 'https://github.com/OpenSocket/socketcluster-client-ruby'
+                  },
+                  {
+                      image: 'https://seeklogo.com/images/U/unity-logo-988A22E703-seeklogo.com.png',
+                      imageAlign: 'top',
+                      title: 'Unity Client',
+                      imageLink: 'https://github.com/sacOO7/socketcluster-client-unity'
+                  },
+              ]}
+          </BlockWithHeading>
+      );
     // const Showcase = () => {
     //   const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
     //   const langPart = `${language ? `${language}/` : ''}`;
@@ -231,6 +286,9 @@ class Index extends React.Component {
         <div className="mainContainer">
           <Features />
         </div>
+          <div className="mainContainer">
+              <ClientLibraries />
+          </div>
       </div>
     );
   }

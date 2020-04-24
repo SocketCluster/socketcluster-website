490b5fbb7e81941a36d8f58304a830f489309af3
Sylvain Pace <sylvain.pace@algolia.com>
2020-04-24 11:05:06 +0200

    feat(website): add DocSearch as recommended by docusaurus

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,6 +21,13 @@ const users = [
 ];
 
 const siteConfig = {
+  algolia: {
+    apiKey: 'ddd1c50927f4bfa6b4cdf793752ed63e',
+    indexName: 'socketcluster',
+    algoliaOptions: {
+      facetFilters: [ "version:VERSION" ]
+    }
+  },
   title: 'SocketCluster', // Title for your website.
   tagline: 'Highly scalable realtime framework optimized for async/await',
   url: 'https://socketcluster.io', // Your website URL
